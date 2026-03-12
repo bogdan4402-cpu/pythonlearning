5a06dcf9a08411d90fdf4127dab80f3a864de9fe
bogdan4402-cpu <bogdan4402@proton.me>
2026-03-13 00:05:49 +0200

    урок 16 оновлений

--- основи/урок 16.py
+++ основи/урок 16.py
@@ -1,6 +1,12 @@
 import random
+import time
 words = "Звісно","Так","50 на 50","Всяке може бути","Звичайно так!","Я в тебе вірю" 
+colors = ["\033[92m", "\033[91m", "\033[94m", "\033[95m"]
 while True:
-    input("Запитай мене про щось: ")
-    print(random.choice(words))
-
+    user_query = input("Запитай мене про щось: ")
+    c = random.choice(colors)
+    time.sleep(1)
+    print(f"{c}{random.choice(words)}\033[0m")
+    user_query.lower() == 'стоп'
+    break
+    
