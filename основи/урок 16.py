f10d404c50da2ed23c51c9a47fcf33c168cc97f5
bogdan4402-cpu <bogdan4402@proton.me>
2026-03-12 23:39:35 +0200

    Урок 16

---
+++ основи/урок 16.py
@@ -0,0 +1,6 @@
+import random
+words = "Звісно","Так","50 на 50","Всяке може бути","Звичайно так!","Я в тебе вірю" 
+while True:
+    input("Запитай мене про щось: ")
+    print(random.choice(words))
+
