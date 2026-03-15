83127ef83d79cfbeb0ecde7432a545a9caf569ff
bogdan4402-cpu <bogdan4402@proton.me>
2026-03-15 11:09:07 +0200

    Урок 17 частина 3

--- основи/урок 17 частина 2.py
+++ основи/урок 17 частина 2.py
@@ -1,15 +1,33 @@
 import time
 import os
+def save_note(text):    
+    try:
+        with open("notes.txt", "a") as file:
+            file.write(text + "\n")
+        return True
+    except Exception:
+        return False
+       
+
 while True:
-    note = input("Введи нотатку (Або 'стоп' для виходу: ")
+    note = input("Введи нотатку (або 'стоп' для виходу): ")
     if note.lower() == "стоп":
         break
-    current_time = time.ctime()
     
-    with open("notes.txt", "a") as file:
-        file.write(f"[{current_time}] {note}\n")
+    current_time = time.ctime() # Отримуємо час тут
+     # Викликаємо нашу функцію і передаємо їй сформований текст 
+if save_note(f"[{current_time}] {note}"):
+        print("Записано!")
+else:
+    print("Помилка у записі файлу")
+
+print("\n--- Твій файл --- ")
+try:
+    with open("notes.txt", "r") as file:
+        print(file.read())
+    os.startfile("notes.txt")
+except FileNotFoundError:
+    print("Упс! Схоже файл  notes.txt кудись зник!")
     
-    print("Записано!")
-os.startfile("notes.txt")
    
         
